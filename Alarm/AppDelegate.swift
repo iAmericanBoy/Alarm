3c6f09d74b8a410573c6085e67df55c01bd5dd0b
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-01-29 14:33:09 -0700

    Adds permission request for UserNotification

--- Alarm/AppDelegate.swift
+++ Alarm/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            if granted {
+                print("permission has been granted")
+            } else {
+                print("permission has not been granted")
+            }
+            if let error = error {
+                print("An error has occured:\(String(describing: error.localizedDescription))")
+            }
+        }
         AlarmController.shared.loadFromPersistantStorage()
         return true
     }
